00758198730c1ea3409519cfd46f3ecc8d1285c4
Neuromechanics and Bioinspired Technologies Lab <h.lin@imperial.ac.uk>
2026-08-03 07:47:43 +0800

    visual tracking eval
    new tuning2 and pantest do not call PID which is simpler. visTrack.py is a simple target following example. however, the servo motor controller setting is require as there seems to be very serious deadband.

--- Assignment 2/pantest.py
+++ Assignment 2/pantest.py
@@ -1,15 +1,20 @@
-from tuning import *
+from tuning2 import *
 from machine import LED
 
 led = LED("LED_BLUE")
 led.on()
+print("LED on")
 
 thresholds = [
       (44, 70, 30, 87, 22, 75), # Red
       (19, 44, 20, 68, -94, -48), # Blue
       (49, 80, -80, -15, -2, 78), # Green
 ]
+print("Thresholds defined")
 
-tuning = PanTuning(thresholds, gain = 10, p=1, i=0.05, d=0.05, imax=0)
+# No PID
+tuning2 = PanTuning(thresholds, gain=10)
+print("PanTuning created")
 
-tuning.measure(0.8)
+tuning2.measure(0.5)
+print("measure called")
